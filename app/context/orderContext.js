a2964bef25a32331e1eec0f56bdc0a3071ac281c
KaiqueFj <kaiquelferraz@hotmail.com>
2025-09-11 23:34:10 -0300

    feat: added orderContext

---
+++ app/context/orderContext.js
@@ -0,0 +1,29 @@
+const { createContext, useReducer } = require("react");
+
+const initialState = {
+  orderItems: [],
+  orderId: null,
+  orderStatus: null,
+};
+
+const OrderContext = createContext();
+
+function OrderProvider({ children }) {
+  const [state, dispatch] = useReducer(orderReducer, initialState);
+
+  return (
+    <OrderContext.Provider value={{ state, dispatch }}>
+      {children}
+    </OrderContext.Provider>
+  );
+}
+
+function useOrder() {
+  const context = useContext(OrderContext);
+  if (!context) {
+    throw new Error("useOrder must be used within a OrderProvider");
+  }
+  return context;
+}
+
+export { OrderProvider, useOrder };
