de83d1cdd75f8c842df833b89b0961464ac2e948
KaiqueFj <kaiquelferraz@hotmail.com>
2025-09-15 21:22:38 -0300

    style: improved the style of not found page

--- app/not-found.js
+++ app/not-found.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 function NotFound() {
   return (
     <main className="mt-4 space-y-6 text-center">
-      <h1 className="text-3xl font-semibold">
+      <h1 className="text-3xl text-primary-500 font-semibold">
         This page could not be found :(
       </h1>
       <Link
         href="/"
-        className="inline-block px-6 py-3 text-lg bg-accent-500 text-primary-800"
+        className="inline-block px-6 py-3 text-lg bg-accent-500 text-primary-500/70 hover:underline"
       >
         Go back home
       </Link>
