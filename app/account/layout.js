813fe712dbdce20f9e52f77b5d15515a5d94aa44
KaiqueFj <kaiquelferraz@hotmail.com>
2025-11-13 23:46:33 -0300

    feat: added a new layout for the account page, with a better view, redirects and user friendly

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,9 +1,6 @@
-import SideNavigation from "../_components/sideNavigation/SideNavigation";
-
 export default function layout({ children }) {
   return (
-    <div className="grid grid-cols-[16rem_1fr] text-primary-600  gap-12 h-full">
-      <SideNavigation />
+    <div className=" flex flex-col justify-center self-center text-primary-600  gap-12 h-full">
       <div className="py-1">{children}</div>
     </div>
   );
--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,4 @@
+import AccountMenu from "../_components/sideNavigation/Navigation";
 import { auth } from "../_lib/auth";
 
 export const metadata = {
@@ -9,8 +10,18 @@ export default async function page() {
   const firstName = session?.user?.name?.split(" ")[0];
 
   return (
-    <h2 className="text-2xl font-semibold text-accent-400 mb-7">
-      Welcome, {firstName}
-    </h2>
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col p-4 items-center ">
+        <h2 className="text-2xl font-semibold text-accent-400 mb-7">
+          Welcome, {firstName}
+        </h2>
+        <p className="text-lg text-center text-primary-600/60">
+          Take a look at your account details, recent orders and manage your
+          profile
+        </p>
+      </div>
+
+      <AccountMenu />
+    </div>
   );
 }
