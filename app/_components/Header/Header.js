edf77058eafbae3ea5aaa277d134fad32612027f
KaiqueFj <kaiquelferraz@hotmail.com>
2025-11-14 23:03:15 -0300

    feat: added the style to make the header still on top even if the user scroll down

--- app/_components/Header/Header.js
+++ app/_components/Header/Header.js
@@ -5,7 +5,7 @@ import SearchBar from "./otherOptions/SearchBar";
 
 export default function Header() {
   return (
-    <header className="w-full border-b-4 border-primary-200/60 px-5 py-4">
+    <header className="w-full border-b-4 border-primary-200/60 px-5 py-4 sticky top-0 bg-white z-50">
       <div className="flex flex-row justify-between gap-2 items-center  max-w-7xl mx-auto">
         <Logo />
         <Navigation />
