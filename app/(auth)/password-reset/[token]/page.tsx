85754a3197cf910f27760447b5814359563fbd0f
KaiqueFj <kaiquelferraz@hotmail.com>
2026-02-11 15:25:08 -0300

    feat: auth folder  files from js -> tsx

--- app/(auth)/password-reset/[token]/page.tsx
+++ app/(auth)/password-reset/[token]/page.tsx
@@ -1,6 +1,6 @@
 import ResetPasswordForm from "@/app/_components/auth/PasswordReset/FormToResetPassword";
 
-export default async function page({ params }) {
+export default async function page({ params }: { params: { token: string } }) {
   const { token } = await params;
 
   return <ResetPasswordForm token={token} />;
