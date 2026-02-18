8c4945a5ef08533a7f8415fdd0bf945207599824
KaiqueFj <kaiquelferraz@hotmail.com>
2026-02-18 01:43:02 -0300

    feat: improved the header search bar

--- app/(products)/sneakers/nav/loading.js
+++
@@ -1,5 +0,0 @@
-import Spinner from "@/app/_components/ui/Spinner/Spinner";
-
-export default function loading() {
-  return <Spinner size="lg" label="Loading sneakers..." />;
-}
---
+++ app/(products)/sneakers/nav/loading.tsx
@@ -0,0 +1,5 @@
+import Spinner from "@/app/_components/ui/Spinner/Spinner";
+
+export default function loading(): React.ReactElement {
+  return <Spinner size="lg" label="Carregando tênis..." />;
+}
